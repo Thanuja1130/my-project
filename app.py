76e50f91afff44645a7f72c88369f0e9f53eae09
Thanuja <thanujakamatham3@gmail.com>
2026-07-21 21:33:22 -0500

    commiting docker file

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+
+if __name__ == "__main__":
+    print("Welcome to Version 2!")
+    print("Addition:", add(20, 10))
+    print("Subtraction:", subtract(20, 10))
